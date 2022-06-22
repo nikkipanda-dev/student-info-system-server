3db779cbbea39c124d02556d55f5eae57fd1c75c
Nikki <62213020+nikkipanda-dev@users.noreply.github.com>
2022-06-22 19:27:47 +0800

    test api prod

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get("/test", [AuthController::class, "test"]);
+
 Route::post("/login", [AuthController::class, "authenticate"]);
 
 Route::middleware(['auth:sanctum'])->group(function () {
